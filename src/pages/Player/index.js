f70e56396114f684b80e526455a9b299071494e3
Janilson Mendes <janilson.mendes@gmail.com>
2023-08-27 18:58:08 -0300

    add
    Adicionando o projeto

---
+++ src/pages/Player/index.js
@@ -0,0 +1,46 @@
+import { useParams } from "react-router-dom";
+import Banner from "components/Banner";
+import Titulo from "components/Titulo";
+
+import styles from "./Player.module.css";
+import NaoEncontrada from "pages/NaoEncontrada";
+import { useEffect, useState } from "react";
+
+function Player() {
+  const parametros = useParams();
+  const [video, setVideo] = useState([]);
+
+  useEffect(() => {
+      fetch(`https://my-json-server.typicode.com/janilson/alura-cine-tag-api/videos?id=${parametros.id}`)
+      .then(resposta => resposta.json())
+      .then(dados => {
+        setVideo(...dados);
+      });
+  }, []);
+
+  if (!video) {
+    return <NaoEncontrada />;
+  }
+
+  return (
+    <>
+      <Banner imagem="player" />
+      <Titulo>
+        <h1>Player</h1>
+      </Titulo>
+      <section className={styles.container}>
+        <iframe
+          width="560"
+          height="315"
+          src={video.link}
+          title={video.titulo}
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </section>
+    </>
+  );
+}
+
+export default Player;
